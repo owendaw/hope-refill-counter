f3d99f1ce84329136e9bcba03533868ada74b1f0
owendaw <owendaw@gmail.com>
2023-06-08 18:49:37 -0400

    clean up

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from 'react';
 import LeftContainer from './LeftContainer';
 import RightContainer from './RightContainer';
 import '../index.css';
-// import { fetchBottles, fetchEmissions } from '../services/api';
 import { fetchFluidOunces } from '../services/api';
 
 function App() {
@@ -24,19 +23,8 @@ function App() {
       }
     }, 5000);
 
-    // const fetchEmissionsInterval = setInterval(async () => {
-    //   try {
-    //     const data = await fetchEmissions();
-    //     setEmissions(data);
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // }, 5000);
-
     return () => {
       clearInterval(fetchFluidOuncesInterval);
-      // clearInterval(fetchBottlesInterval);
-      // clearInterval(fetchEmissionsInterval);
     };
   }, []);
 
