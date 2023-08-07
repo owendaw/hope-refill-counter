b3a3c992e4112240a13faf3362ba050d770db85a
owendaw <owendaw@gmail.com>
2023-08-07 13:54:39 -0400

    Update Website background and Carbon Counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import BottleCounter from './components/BottleCounter';
 import './App.css';
-import horizontalBackground from './assets/horizontalBackground.png';
-import verticalBackground from './assets/verticalBackground.png';
 
 function App() {
   return (
